27f9cdc39105904b244d9e1fd9bbc5e6ec5ce16b
mujinyun <mujinyun2009@gmail.com>
2020-03-23 20:23:03 +0000

    Change game_files

--- stats/data.py
+++ stats/data.py
@@ -8,7 +8,7 @@ game_files.sort()
 
 game_frames = []
 
-for game_file in games_files:
+for game_file in game_files:
     game_frame = pd.read_csv(game_file, names=['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'])
     game_frames.append(game_frame)
 
